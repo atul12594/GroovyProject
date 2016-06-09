8241b6481a05594af3e89abaebbf2727236be1d7
Atul Aggarwal <atulaggarwal12594@gmail.com>
2016-06-09 12:32:47 +0530

    Added to git

--- src/groovy5.groovy
+++ src/groovy5.groovy
@@ -1,5 +1,21 @@
 /**
  * Created by atul on 9/6/16.
  */
-class groovy5 {
+List a = [20, 23, 30, 35, 40, 50 ]
+
+List b = []
+
+a.each { item ->
+    if(item > 20) {
+        b.push item
+    }
 }
+ b = a.findAll {
+     return it > 20
+ }
+
+println a.find {
+    return true
+}
+
+println b
